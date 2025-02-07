7b76232f1faccc700b1e0df182b2b6e298568f26
kayan <kayan.hyatt@gmail.com>
2025-02-07 17:04:10 +0000

    Created the Login and Signup pages
    Still require the forms to link to the data base but there isnt one right now

--- ECM2434/Ecolution/urls.py
+++ ECM2434/Ecolution/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-
 from . import views
+from .views import login_view, signup_view
+
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path('login/', login_view, name='login'),
+    path('signup/', signup_view, name='signup'),
 ]
--- ECM2434/Ecolution/views.py
+++ ECM2434/Ecolution/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world. You're at the Ecolution index")
+
+def login_view(request):
+    return render(request, 'login.html')
+
+def signup_view(request):
+    return render(request, 'signup.html')
