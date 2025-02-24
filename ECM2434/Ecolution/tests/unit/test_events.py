696c54d7bdc4bce75d698173938ef99f706fcfd0
tkostest1 <tester1@tkos.co.uk>
2025-02-24 12:19:18 +0000

    Deleted map tests

--- ECM2434/Ecolution/tests/unit/test_events.py
+++ ECM2434/Ecolution/tests/unit/test_events.py
@@ -13,21 +13,17 @@ class EventsTestCase(TestCase):
         self.client.login(username='testuser', password='password')
         response = self.client.get(reverse('home'))
 
+        # user is redirected to events page
+        response = self.client.get('/events/')
+
     ## As a user, I can view the Events page
     def test_view_events(self):
-        self.client.login(username = 'testuser', password = 'password')
+        # user is redirected to events page
         response = self.client.get('/events/')
 
         # check user is redirected to "events" page
         self.assertEqual(response.status_code, 200)
         self.assertRedirects(response, '/events/')
-
-    ## As a user, I can search for events
-    ## As a user, I can view event details
-    ## As a user, I can add events to my list
-    ## As a user, I can remove events from my list
-    ## As a user, I can view events on the map
-    ## As a user, I can complete events
 
     ## As a user, I can open and close the menu
     def test_events_menu(self):
@@ -36,3 +32,9 @@ class EventsTestCase(TestCase):
 
         # menu opens up
         self.assertContains(response, 'menu')
+
+        # user deselects menu button
+        response = self.client.post(reverse('events'), {'show menu': False}, follow = False)
+
+        # menu closes
+        self.assertNotContains(response, 'menu')
--- ECM2434/Ecolution/tests/unit/test_settings.py
+++ ECM2434/Ecolution/tests/unit/test_settings.py
@@ -6,29 +6,18 @@ from Ecolution.models import Task
 
 class SettingsTestCase(TestCase):
     def setUp(self):
-        self.user = CustomUser.objects.create_user(username = 'testuser', password = 'password') # create user
-        self.client.login(username = 'testuser', password = 'password') # login user
+        # create user
+        self.user = CustomUser.objects.create_user(username = 'testuser', password = 'password')
 
-    ## As a user, I can change my username
-    def test_settings_change_username(self):
+        # login user
         self.client.login(username = 'testuser', password = 'password')
+
+        # user navigates to settings page
         response = self.client.get(reverse('settings'))
 
-        # user enters new username
-        # TODO: enter correct username change url
-        # self.client.post(reverse(/username_url/), 'notatestuser')
-
-    ## As a user, I can change my pet's name
-    def test_settings_change_petname(self):
-        self.client.login(username = 'testuser', password = 'password')
+    ## As a user, I can open and close the navigation menu
+    def test_settings_menu(self):
         response = self.client.get(reverse('settings'))
+        self.assertEqual(response.status_code, 200)
 
-        # TODO: user enters new petname
-
-
-    ## As a user, I can reset my points (?)
-    def test_settings_reset_points(self):
-        self.client.login(username = 'testuser', password = 'password')
-        response = self.client.get(reverse('settings'))
-
-        # TODO: user selects "reset points" button (?)
+        # user selects menu
