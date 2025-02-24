d181aa700761640aac7864d4e0371c96bf1b2f98
tkostest1 <tester1@tkos.co.uk>
2025-02-24 16:35:20 +0000

    Updated tests

--- ECM2434/Ecolution/tests/unit/test_tasks.py
+++ ECM2434/Ecolution/tests/unit/test_tasks.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from Ecolution.models import CustomUser
+from Ecolution.models import CustomUser, Pet
 from Ecolution.models import UserTask
 from Ecolution.models import Task
 
@@ -12,6 +12,16 @@ class TasksUnitTests(TestCase):
         # create a test task
         self.task1 = Task.objects.create(task_name = "Buy groceries", description = "Go to the store and buy food")
         self.task2 = Task.objects.create(task_name = "Task 2", description = "Task description here")
+        self.task3 = Task.objects.create(task_name = "Task 3", description = "Task description here", xp_given = 100)
+
+        # create user's pet
+        self.pet1 = Pet.objects.create(
+            user = self.user1,
+            pet_name = "TestPet",
+            pet_level = 1,
+            pet_exp = 0,  # pet starts with 0 XP
+            pet_type = "mushroom"
+        )
 
     ## As a user, I can add (pre-defined) tasks to my list
     def test_user_adds_tasks(self):
@@ -56,4 +66,28 @@ class TasksUnitTests(TestCase):
 
     ## As a user, I can earn points from completing tasks
     def test_user_earns_points(self):
+        # check user xp before completing task
+        user_xp_before = self.pet1.pet_exp
+        print(f"XP Before: {user_xp_before}")  # TODO: debug statement
 
+        # add task to user list
+        user_tasks = UserTask.objects.create(user = self.user1, task = self.task3)
+
+        # check task is now in user's current tasks list
+        self.assertTrue(UserTask.objects.filter(user = self.user1).exists())
+        self.assertTrue(UserTask.objects.filter(task = self.task3, completed = False).exists())
+
+        # user marks task as "complete"
+        user_tasks.completed = True
+        user_tasks.save()
+
+        # task no longer appears in current tasks list
+        self.assertFalse(UserTask.objects.filter(task = self.task3, completed = False).exists())
+
+        # task now appears in completed tasks list
+        self.assertTrue(UserTask.objects.filter(task = self.task3, completed = True).exists())
+
+        # check points have been awarded
+        user_exp_after = self.pet1.pet_exp
+        print(f"XP After: {user_exp_after}")  # TODO: debug statement
+        self.assertTrue(user_xp_before < user_exp_after)
