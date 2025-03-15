8c11d23d27d739d343cb0bf4622acc489c3030cd
tkostest1 <tester1@tkos.co.uk>
2025-03-15 23:49:22 +0000

    Added initial tests for Leaderboard page

--- ECM2434/Ecolution/tests/integration/test_integration_shop.py
+++ ECM2434/Ecolution/tests/integration/test_integration_shop.py
@@ -1,13 +1,42 @@
 from django.test import TestCase
 from django.urls import reverse
-from django.db import connection
-from Ecolution.models import CustomUser, Event, Pet
+
+from Ecolution.models import CustomUser, Event, Pet, ShopItem
+
 
 class ShopIntegrationTest(TestCase):
     def setUp(self):
         self.user1 = CustomUser.objects.create_user(username='testuser', password='password')
-        self.client.login(username='testuser', password='password')
+
+        self.client.login(username = 'testuser', password = 'password')
+
+        # add points to the user
+        self.user1.points += 50
+        self.user1.save()  # save to the database (user now has 50 points)
+
+        # refresh from the database
+        self.user1.refresh_from_db()
+
+        # create new item in shop
+        self.item1 = ShopItem.objects.create(
+            name = 'item1',
+            price = 10
+        )
 
     # As a user, I can purchase an item multiple times after a cooldown period
 
     # As a user, I can purchase an item, and the points are removed from my account
+    def test_shop_user_points_decrease(self):
+        # check user starts with 50 points
+        self.assertEqual(self.user1.points, 50)
+
+        # user buys shop item
+        response = self.client.post(reverse('buy_item', args = [self.item1.id]))
+        self.assertEqual(response.status_code, 200)
+        print(response.content)
+
+        # refresh from the database
+        self.user1.refresh_from_db()
+
+        # check user now has 40 points
+        self.assertEqual(self.user1.points, 40)
--- ECM2434/Ecolution/tests/unit/test_leaderboard.py
+++ ECM2434/Ecolution/tests/unit/test_leaderboard.py
@@ -5,15 +5,52 @@ from Ecolution.models import CustomUser, Event, Pet
 
 class TestLeaderboard(TestCase):
     def setUp(self):
+        # create users and login new user
+        self.user1 = CustomUser.objects.create_user(username='testuser', password='password')
+        self.user2 = CustomUser.objects.create_user(username='user2', password='password')
+        self.user3 = CustomUser.objects.create_user(username='user3', password='password')
+        self.client.login(username='testuser', password='password')
+
+        # create pets for users
+        self.pet1 = Pet.objects.create(user=self.user1, pet_name='Test Pet', pet_exp = 11)
+        self.pet2 = Pet.objects.create(user=self.user1, pet_name='Test Pet', pet_exp=17)
+        self.pet3 = Pet.objects.create(user=self.user1, pet_name='Test Pet', pet_exp=13)
 
     # As a user, I can access the Leaderboard page
+    def test_view_leaderboard(self):
+        response = self.client.get(reverse('leaderboard'))
+        self.assertEqual(response.status_code, 200)
 
-    # As a user, I can see myself on the Leaderboard
+    # As a user, I can see myself (via username) on the Leaderboard
+    def test_leaderboard_view_self(self):
+        # user navigates to leaderboard
+        response = self.client.get(reverse('leaderboard'))
 
-    # As a user, I can see my username on the card
+        # check user's name appears on leaderboard
+        self.assertContains(response, 'testuser')
 
     # As a user, I can see the level of my pet on the card
+    def test_leaderboard_view_pet_level(self):
+        response = self.client.get(reverse('leaderboard')
+
+        # check pet level appears
+        self.assertContains(response, self.pet1.pet_exp)
 
     # As a user, I can see the name of my pet on the card
+    def test_leaderboard_view_pet_name(self):
+        response = self.client.get(reverse('leaderboard'))
+
+        # check pet name appears
+        self.assertContains(response, self.pet1.pet_name)
 
     # As a user, I can see other users cards
+    def test_leaderboard_view_other_users(self):
+        response = self.client.get(reverse('leaderboard'))
+
+        # check other users and their pets appear
+        self.assertContains(response, self.user2.username)
+        self.assertContains(response, self.user3.username)
+        self.assertContains(response, self.pet2.pet_name)
+        self.assertContains(response, self.pet3.pet_name)
+        self.assertContains(response, self.pet2.pet_exp)
+        self.assertContains(response, self.pet3.pet_exp)
