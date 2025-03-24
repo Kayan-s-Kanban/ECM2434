78593e5edc78b76f9fb81afee137ae214dd3b8e4
Dillon O'Connor <37981696+DillonOC@users.noreply.github.com>
2025-03-24 19:32:23 +0000

    Update apps.py

--- ECM2434/Ecolution/apps.py
+++ ECM2434/Ecolution/apps.py
@@ -1,5 +1,10 @@
+import os
 from django.apps import AppConfig
 
 class EcolutionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Ecolution'
+
+     def ready(self):
+        # Ensure the MEDIA_ROOT folder exists
+        os.makedirs(settings.MEDIA_ROOT, exist_ok=True)
