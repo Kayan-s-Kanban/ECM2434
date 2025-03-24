53a3989b8c6bc78e7dff6d160c357e5b6b785db4
Dillon O'Connor <37981696+DillonOC@users.noreply.github.com>
2025-03-24 16:27:39 +0000

    Finishing media server

--- ECM2434/ECM2434/settings.py
+++ ECM2434/ECM2434/settings.py
@@ -56,7 +56,7 @@ AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME', 'eu-north-1')  # updat
 
 AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None  # This can be 'public-read' if you want public files
+AWS_DEFAULT_ACL = 'public-read'
 
 # Use S3 for media storage:
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
