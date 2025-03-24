a36d64d12e0503992a524c9e103bd48c1b05e597
Dillon O'Connor <37981696+DillonOC@users.noreply.github.com>
2025-03-24 19:35:12 +0000

    indent

--- ECM2434/Ecolution/apps.py
+++ ECM2434/Ecolution/apps.py
@@ -5,6 +5,6 @@ class EcolutionConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Ecolution'
 
-     def ready(self):
+    def ready(self):
         # Ensure the MEDIA_ROOT folder exists
         os.makedirs(settings.MEDIA_ROOT, exist_ok=True)
